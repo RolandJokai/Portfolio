4dfd1b8e6d750454f4511a1ab0894b898d268956
RolandJókai <jokairoland@gmail.com>
2025-04-05 18:20:07 +0200

    Portfolio 2025.04.05 Routing pipa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroSiteComponent } from './hero-site/hero-site.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"hero-site",component:HeroSiteComponent},
+  {path:"portfolio",component:PortfolioComponent},
+  {path:"contact",component:ContactComponent},
+
+  {path:"",redirectTo:"/hero-site",pathMatch:"full"},
+
+  {path:"**",component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroSiteComponent } from './hero-site/hero-site.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorComponent } from './error/error.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroSiteComponent
+    HeroSiteComponent,
+    PortfolioComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
