3be7823924a8c9d94e74f54a8abb9b5461ac1383
RolandJókai <jokairoland@gmail.com>
2025-04-06 13:27:20 +0200

    2025.04.06 Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Portfolio';
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
